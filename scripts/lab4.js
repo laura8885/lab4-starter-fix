ffa9239bdd4c206af094afbef7853afd9e18ae6a
Laura <laurakarry82@gmail.com>
2026-04-25 18:09:53 -0700

    fix buggy lab code

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -7,14 +7,14 @@
  */
 function sumValues(num1, num2, add) {
     if (add) {
-        const result = 0;
+        let result = 0;
 
         result = num1 + num2;
 
         return result;
     }
     else {
-        return !add;
+        return false;
     }
 }
 
@@ -25,15 +25,20 @@ function sumValues(num1, num2, add) {
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
 function discountPrices(prices, discount) {
-    const discounted = []
+    const discounted = [];
+
+    if (prices.length === 0) {
+        return false;
+    }
+
     const length = prices.length;
-    let discountedPrice = 0
-    for(let i = 0; i < length; i++) {
-        discountedPrice += prices[i] * (1 - discount);
+
+    for (let i = 0; i < length; i++) {
+        let discountedPrice = prices[i] * (1 - discount);
         discounted.push(discountedPrice);
     }
 
     return discounted;
 }
 
-module.exports = {sumValues, discountPrices};
+module.exports = { sumValues, discountPrices };
